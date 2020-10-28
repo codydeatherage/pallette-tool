7b35cba7bdf6b65fe59a8a44cc40bf7fb326ad6e
codydeatherage <cody@DESKTOP-DCKI10R.localdomain>
2020-10-27 23:09:43 -0700

    added buttons

--- app.js
+++ app.js
@@ -10,24 +10,10 @@ const bot_left = document.getElementById('bot-left');
 const top_right = document.getElementById('top-right');
 const bot_right = document.getElementById('bot-right');
 
-top_left.addEventListener('click' , () =>{
+/* top_left.addEventListener('click' , () =>{
     let color = getRandomColor();
     top_left.innerText = color;
     top_left.style.backgroundColor = color;
 })
+ */
 
-bot_left.addEventListener('click' , () =>{
-    let color = getRandomColor();
-    bot_left.innerText = color;
-    bot_left.style.backgroundColor = color;
-})
-top_right.addEventListener('click' , () =>{
-    let color = getRandomColor();
-    top_right.innerText = color;
-    top_right.style.backgroundColor = color;
-})
-bot_right.addEventListener('click' , () =>{
-    let color = getRandomColor();
-    bot_right.innerText = color;
-    bot_right.style.backgroundColor = color;
-})
